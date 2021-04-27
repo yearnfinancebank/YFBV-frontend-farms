f30c2af45a03f02b438ea3e0d19bd074ce7849f8
Yearn Finance Bank Vault <80194658+yearnfinancebank@users.noreply.github.com>
2021-04-27 21:43:01 +0800

    Update colors.ts

--- src/uikit/theme/colors.ts
+++ src/uikit/theme/colors.ts
@@ -1,35 +1,35 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ED4B9E",
-  primary: "#dec9cb",
-  primaryBright: "#df465d",
-  primaryDark: "#df6779",
-  secondary: "#fa607f",
-  success: "#e9a0a5",
-  warning: "#FFB237",
+  failure: "#17f0ff",
+  primary: "#19defa",
+  primaryBright: "#000000",
+  primaryDark: "#a8a89e",
+  secondary: "#52fae9",
+  success: "#63fff7",
+  warning: "#56f5ef",
 };
 
 export const brandColors = {
-  binance: "#F0B90B",
+  binance: "#56f5ef",
 };
 
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#fcd9d3",
-  background: "#c35068",
-  backgroundDisabled: "#3c3742",
-  contrast: "#FFFFFF",
-  invertedContrast: "#191326",
-  input: "#483f5a",
+  secondary: "#4ee8fc",
+  background: "#fefffa",
+  backgroundDisabled: "#ffffff",
+  contrast: "#ffffff",
+  invertedContrast: "#ffffff",
+  input: "#ffffff",
   primaryDark: "#0098A1",
-  tertiary: "#353547",
-  text: "#dbcfd1",
-  textDisabled: "#666171",
-  textSubtle: "#c9c4d4",
-  borderColor: "#524B63",
-  card: "#852f41",
+  tertiary: "#ffffff",
+  text: "#000000",
+  textDisabled: "#000000",
+  textSubtle: "#000000",
+  borderColor: "#ffffff",
+  card: "#ffc60d",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
@@ -41,16 +41,15 @@ export const darkColors: Colors = {
   background: "#001113",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
-  invertedContrast: "#FFFFFF",
+  invertedContrast: "#53fcf1",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#ecbaae",
-  textDisabled: "#BDC2C4",
-  textSubtle: "#e1b7ad",
-  borderColor: "#E9EAEB",
+  text: "#4ee8fc",
+  textDisabled: "#4ee8fc",
+  textSubtle: "#4ee8fc",
+  borderColor: "#4ee8fc",
   card: "#010f11",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
   },
 };
-
